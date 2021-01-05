a9a91bd3789f85239c804e4c6441379948597f73
Xavier Genolhac <x.genolhac@gmail.com>
2021-01-05 15:18:40 +0100

    shadowRoot.innerHTML with Lior CHAMLA

--- component/meteo-widget.component.js
+++ component/meteo-widget.component.js
@@ -3,6 +3,15 @@ class MeteoWidgetElement extends HTMLElement {
     constructor() {
         super()
         this.attachShadow({mode: 'open'})
+
+        this.shadowRoot.innerHTML = `
+            <img src ="">
+            <h2>Paris</h2>
+            <h3>
+                <span id="temperature">25°C</span>
+                (<span id="description">Nuageux</span>)
+            </h3>
+        `
     }
 
     fetchAPIDATA() {
