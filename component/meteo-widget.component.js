69a2637dc4ddb56ada97f6f4e51a6c149fa8c402
Xavier Genolhac <x.genolhac@gmail.com>
2021-01-05 17:33:04 +0100

    function render() work with Lior CHAMLA

--- component/meteo-widget.component.js
+++ component/meteo-widget.component.js
@@ -14,18 +14,38 @@ class MeteoWidgetElement extends HTMLElement {
         `
     }
 
-    fetchAPIDATA() {
-
+    fetchAPIData() {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this._city}
+        &units=${this._units}&lang=${this._lang}&appid=67bb7169db829dd621bc83b830d71486`)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+                this._temperature=json.main.temp
+                this._description=json.weather[0].description
+                this._icon=json.weather[0].icon
+                this.render()
+            })
     }
 
     connectedCallback() {
         this._city = this.getAttribute('city') || 'Paris'
         this._units = this.getAttribute('units') || 'metric'
         this._lang = this.getAttribute('lang') || 'fr'
+
+        this.fetchAPIData();
     }
 
     render () {
-
+        this.shadowRoot.querySelector('img').src =
+        `http://openweathermap.org/img/wn/${this._icon}.png`
+        
+        this.shadowRoot.querySelector('h2').textContent = this._city
+        
+        this.shadowRoot.querySelector('h3 #temperature').textContent = 
+        this._temperature + (this._units == 'metric' ? ' °C' : '°F')
+        
+        this.shadowRoot.querySelector('h3 #description').textContent = `
+            ${this._description}`
     }
 }
 
