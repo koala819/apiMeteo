0bcd5b933f1da731d30f8a9093d7d6f61730606b
Xavier Genolhac <x.genolhac@gmail.com>
2021-01-05 15:00:03 +0100

    initialize API meteo with Lior CHAMLA

---
+++ component/meteo-widget.component.js
@@ -0,0 +1,21 @@
+class MeteoWidgetElement extends HTMLElement {
+
+    constructor() {
+        super()
+        this.attachShadow({mode: 'open'})
+    }
+
+    fetchAPIDATA() {
+
+    }
+
+    connectedCallback() {
+
+    }
+
+    render () {
+
+    }
+}
+
+window.customElements.define('meteo-widget', MeteoWidgetElement)
